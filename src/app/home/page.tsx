88ff49f5bc1759238e2021b84a37f1c83bdfa19f
Brian Kimble <brian@stickbuilt.us>
2025-09-06 19:47:36 +0000

    For this button remove the Sort By text in the button just have the text

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -249,7 +249,7 @@ export default function HomePage() {
                             <DropdownMenuTrigger asChild>
                                 <Button variant="outline">
                                     <ArrowUpDown className="mr-2 h-4 w-4" />
-                                    Sort by: {sortOption.label}
+                                    {sortOption.label}
                                 </Button>
                             </DropdownMenuTrigger>
                             <DropdownMenuContent align="end">
